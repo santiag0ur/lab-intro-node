9f33a2cdce0453ff7fc58c2aa013aea59f79f048
santiag0ur <83630273+santiag0ur@users.noreply.github.com>
2021-08-11 10:32:22 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,108 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+      if (a === b) {
+        return 0;
+      }
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    switch (pos < this.length) {
+      case true:
+        return this.items[pos];
+        break;
+      case false:
+        throw new Error('OutOfBounds');
+        break;
+    }
+  }
 
-  max() {}
+  max() {
+    switch (this.length > 0) {
+      case true:
+        this.items.sort((a, b) => {
+          if (a > b) {
+            return 1;
+          }
+          if (a < b) {
+            return -1;
+          }
+          if (a === b) {
+            return 0;
+          }
+        });
+        return this.items[this.length - 1];
+        break;
+      case false:
+        throw new Error('EmptySortedList');
+        break;
+    }
+  }
 
-  min() {}
+  min() {
+    switch (this.length > 0) {
+      case true:
+        this.items.sort((a, b) => {
+          if (a > b) {
+            return 1;
+          }
+          if (a < b) {
+            return -1;
+          }
+          if (a === b) {
+            return 0;
+          }
+        });
+        return this.items[0];
+        break;
+      case false:
+        throw new Error('EmptySortedList');
+        break;
+    }
+  }
 
-  sum() {}
+  sum() {
+    switch (this.length > 0) {
+      case true:
+        const total = this.items.reduce(
+          (acumulator, item) => acumulator + item
+        );
+        return total;
+        break;
+      case false:
+        return 0;
+        break;
+    }
+  }
 
-  avg() {}
+  avg() {
+    switch (this.length > 0) {
+      case true:
+        const totalList = this.items.reduce(
+          (acumulator, item) => acumulator + item
+        );
+        const averageList = totalList / this.length;
+        return averageList;
+        break;
+      case false:
+        throw new Error('EmptySortedList');
+        break;
+    }
+  }
 }
 
 module.exports = SortedList;
